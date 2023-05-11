4c7ec1cf1f5bb098c705071ba1a1ed828c766b69
Leonardo Simeone <leo_sime@yahoo.com>
2023-05-11 11:37:42 +0000

    Add blocks to base template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l7bo$9)7e^t$1!$q-h3kue_)bl=@keu7+g0hbrxw$xt1=xj_-u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
